cc54bf963e9a65832325892be07b677b89257924
Anthony Cook <ddragonmaster49@yahoo.com>
2016-07-17 02:21:24 +0000

    Add buttons for passing/ending turn

--- src/js/controls.js
+++ src/js/controls.js
@@ -4,8 +4,10 @@ export default React.createClass({
    render() {
        return(
          <div>
+           <button className="btn btn-success" onClick={this.props.parent.pass}>Pass</button>
            <button className="btn btn-danger" onClick={this.props.parent.undo}>Undo</button>
            <button className="btn btn-warning" onClick={this.props.parent.openExchangeDialog}>Exchange</button>
+           <button className="btn btn-primary" onClick={this.props.parent.endTurn}>End Turn</button>
          </div>
        );
    } 
